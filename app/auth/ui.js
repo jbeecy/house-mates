b18c48a68506842473b539d3b9084ca6decfbc98
jbeecy <john.e.beecy@gmail.com>
2021-10-22 14:54:41 -0400

    reformats posts to show up as modals, not plain text blocks

--- app/auth/ui.js
+++ app/auth/ui.js
@@ -102,10 +102,27 @@ const onIndexPostsSuccess = (response) => {
   let postHTML = ''
   posts.forEach(post => {
     postHTML += `
-    <h4>${post.title}</h4>
-    <h6>${post.location}</h6>
-    <p>${post.body}</p>
-    <p>Post ID: ${post._id}</p>
+    <div class="modal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">${post.title}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+          <h6>${post.location}</h6>
+          <p>${post.body}</p>
+          </div>
+          <div class="modal-footer">
+          <p>Post ID: ${post._id}</p>
+            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
     `
   })
   $('#post-forum').show()
